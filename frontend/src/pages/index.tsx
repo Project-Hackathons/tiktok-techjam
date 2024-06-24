8afc38f3df2c68d168f13b53384486a1724b16b0
Kenneth Gao <108561447+duckyfuz@users.noreply.github.com>
2024-06-24 16:18:40 +0800

    add link, reference to colors

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,8 +1,16 @@
 import Head from "next/head";
+import Link from 'next/link'
+
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+// temp colors reference:
+// black: #010101
+// teal: #69C9D0
+// white: #FFFFFF
+// red: #EE1D52
 
 export default function Home() {
   return (
@@ -14,7 +22,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        
+        <Link href="/map">Withdraw / Deposit</Link>
       </main>
     </>
   );

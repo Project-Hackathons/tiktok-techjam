13481a658654e4538ee9ad0f109e45b01eebab07
Kenneth Gao <108561447+duckyfuz@users.noreply.github.com>
2024-06-25 21:15:18 +0800

    use iife

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -13,15 +13,14 @@ export default function Home() {
   const [userData, setUserData] = useState(null)
 
   useEffect(() => {
-    const fetchData = async () => {
+    (async () => {
       const response = await fetch('http://152.42.182.247:5000/user/1');
       if (!response.ok) {
         throw new Error('Fetching Error');
       }
       const data = await response.json();
       setUserData(data);
-    };
-    fetchData();
+    })()
   }, []);
 
   if (userData === null) {

92b499eb19d77a7e5667db685f42228818ef1d01
euanlimzx <109453826+euanlimzx@users.noreply.github.com>
2024-06-30 16:45:26 +0800

    navbar changes

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -3,6 +3,8 @@ import { useRouter } from "next/router";
 import { IoHomeOutline, IoTrophyOutline } from "react-icons/io5";
 import { FaMoneyBillTransfer } from "react-icons/fa6";
 import { FaMapMarkedAlt } from "react-icons/fa";
+import { MdHistory } from "react-icons/md";
+import { GoHistory } from "react-icons/go";
 
 const Navbar = () => {
   const Router = useRouter();
@@ -10,7 +12,7 @@ const Navbar = () => {
     <Flex bg="#070F2B" position="fixed" bottom="0" zIndex="9999">
       <Flex
         h="4rem"
-        px="2rem"
+        px="1rem"
         py="2rem" //changes based on whether or not we wna include text
         w="100vw"
         shadow="xl"
@@ -39,22 +41,6 @@ const Navbar = () => {
           cursor="pointer"
           _hover={{ bg: "gray.900" }}
           onClick={() => {
-            Router.push("/pay");
-          }}
-          p="1rem"
-          borderRadius="0.5rem"
-        >
-          <VStack gap="0.25rem">
-            <FaMoneyBillTransfer size="1.75rem" color="gray" />
-            {/* <Text color="gray">Transfer</Text> */}
-          </VStack>
-        </Flex>
-        <Flex
-          justifyContent="center"
-          alignItems="center"
-          cursor="pointer"
-          _hover={{ bg: "gray.900" }}
-          onClick={() => {
             Router.push("/map");
           }}
           p="1rem"
@@ -64,6 +50,35 @@ const Navbar = () => {
             <FaMapMarkedAlt size="1.75rem" color="gray" />
             {/* <Text color="gray">Map</Text> */}
           </VStack>
+        </Flex>
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          cursor="pointer"
+          onClick={() => {
+            Router.push("/map");
+          }}
+          p="0.25rem"
+          bgGradient="linear(to-r, #ff0050, 45%, #00f2ea)"
+          borderRadius="0.5rem"
+        >
+          <Flex
+            justifyContent="center"
+            alignItems="center"
+            cursor="pointer"
+            _hover={{ bg: "gray.200" }}
+            onClick={() => {
+              Router.push("/pay");
+            }}
+            bg="white"
+            p="0.5rem"
+            borderRadius="0.5rem"
+          >
+            <VStack gap="0.25rem">
+              <FaMoneyBillTransfer size="1.75rem" />
+              {/* <Text color="gray">Pay</Text> */}
+            </VStack>
+          </Flex>
         </Flex>
         <Flex
           justifyContent="center"
@@ -79,6 +94,22 @@ const Navbar = () => {
           <VStack gap="0.25rem">
             <IoTrophyOutline size="1.75rem" color="gray" />
             {/* <Text color="gray">Rewards</Text> */}
+          </VStack>
+        </Flex>
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          cursor="pointer"
+          _hover={{ bg: "gray.900" }}
+          onClick={() => {
+            Router.push("/history");
+          }}
+          p="1rem"
+          borderRadius="0.5rem"
+        >
+          <VStack gap="0.25rem">
+            <GoHistory size="1.75rem" color="gray" />
+            {/* <Text color="gray">History</Text> */}
           </VStack>
         </Flex>
       </Flex>

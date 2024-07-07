f6138a2ae2d231f017c575deb0ff61d6e4611b03
euanlimzx <109453826+euanlimzx@users.noreply.github.com>
2024-07-07 10:49:50 +0800

    transaction page done !!

--- frontend/src/pages/history.tsx
+++ frontend/src/pages/history.tsx
@@ -1,14 +1,41 @@
-import TransactionList from "@/components/TransactionList"
-import { Flex, VStack, HStack, Button, Text } from "@chakra-ui/react"
+import TransactionList from "@/components/TransactionList";
+import { Flex, VStack, HStack, Button, Text, Skeleton } from "@chakra-ui/react";
+import { User } from "../../types";
+import { useState, useEffect } from "react";
 
 const History = () => {
-    return (
-        <Flex bg="#070F2B" h="100vh" w="100%">
-        <VStack gap="1.5rem" w="100%" pt="1.5rem">
-          <TransactionList />
-        </VStack>
-      </Flex>
-    )
-}
+  const [userData, setUserData] = useState<User | null>(null);
 
-export default History
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("http://152.42.182.247:5000/user/2");
+      if (!response.ok) {
+        throw new Error("Fetching Error");
+      }
+      const data = await response.json();
+      setUserData(data);
+    })();
+  }, []);
+  return (
+    <Flex bg="#070F2B" h="100vh" w="100%">
+      <VStack gap="1.5rem" w="100%" pt="1.5rem">
+        {userData ? (
+          <TransactionList userData={userData} />
+        ) : (
+          <Flex w="80%" h="50%" flexDir="column">
+            <Text color="gray.100" fontSize="x-large" fontWeight="semibold">
+              Transaction History
+            </Text>
+            <VStack color="white" gap="1rem" pt="2rem">
+              <Skeleton height="60px" borderRadius="1rem" w="100%" />
+              <Skeleton height="60px" borderRadius="1rem" w="100%" />
+              <Skeleton height="60px" borderRadius="1rem" w="100%" />
+            </VStack>
+          </Flex>
+        )}
+      </VStack>
+    </Flex>
+  );
+};
+
+export default History;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -6,19 +6,16 @@ import { useRouter } from "next/router";
 
 const Home = () => {
   const [userData, setUserData] = useState<User | null>(null);
-  const [loading, setIsLoading] = useState(true); //todo @euan add skeleton loading states
-  const router = useRouter()
+  const router = useRouter();
 
   useEffect(() => {
     (async () => {
       const response = await fetch("http://152.42.182.247:5000/user/2");
       if (!response.ok) {
-        setIsLoading(true)
         throw new Error("Fetching Error");
       }
       const data = await response.json();
       setUserData(data);
-      setIsLoading(false);
     })();
   }, []);
   return (
@@ -35,57 +32,81 @@ const Home = () => {
           </Text>
         </Flex>
 
-        {
-        loading? <Flex w="80%" flexDir="column"><Skeleton h="8rem" w="100%" borderRadius="1rem" colorScheme="black"/><Skeleton h="15rem" mt="3rem" w="100%" borderRadius="1rem" colorScheme="black"/></Flex>: <>
-        <Flex
-          bgGradient="linear(to-r, #ff0050, 45%, #00f2ea)"
-          w="80%"
-          borderRadius="1rem"
-          p="1rem"
-        >
-          <VStack
-            display="flex"
-            justifyContent="start"
-            alignItems="start"
-            gap="0.5rem"
-            w="100%"
-          >
-            <Text
-              textAlign="start"
-              fontSize="large"
-              fontWeight="semibold"
-              color="white"
+        {!userData ? (
+          <Flex w="80%" flexDir="column">
+            <Skeleton
+              h="8rem"
+              w="100%"
+              borderRadius="1rem"
+              colorScheme="black"
+            />
+            <Skeleton
+              h="15rem"
+              mt="3rem"
+              w="100%"
+              borderRadius="1rem"
+              colorScheme="black"
+            />
+          </Flex>
+        ) : (
+          <>
+            <Flex
+              bgGradient="linear(to-r, #ff0050, 45%, #00f2ea)"
+              w="80%"
+              borderRadius="1rem"
+              p="1rem"
             >
-              Current balance
-            </Text>
-            <Text
-              textAlign="start"
-              fontSize="xx-large"
-              fontWeight="bold"
-              color="white"
-            >
-              SGD ${(Math.round(userData ? userData.balance * 100 : 0) / 100).toFixed(2)}
-            </Text>
-            <HStack w="100%" display="flex">
-              <Button flex={1} variant="solid" _hover={{ bg: "gray.300" }} onClick={()=>router.push("/receive")}>
-                Top-up
-              </Button>
-              <Button
-                flex={1}
-                variant="outline"
-                color="white"
-                _hover={{ bg: "gray.900" }}
-                onClick={()=>router.push("/pay")}
+              <VStack
+                display="flex"
+                justifyContent="start"
+                alignItems="start"
+                gap="0.5rem"
+                w="100%"
               >
-                Withdraw
-              </Button>
-            </HStack>
-          </VStack>
-        </Flex>
+                <Text
+                  textAlign="start"
+                  fontSize="large"
+                  fontWeight="semibold"
+                  color="white"
+                >
+                  Current balance
+                </Text>
+                <Text
+                  textAlign="start"
+                  fontSize="xx-large"
+                  fontWeight="bold"
+                  color="white"
+                >
+                  SGD $
+                  {(
+                    Math.round(userData ? userData.balance * 100 : 0) / 100
+                  ).toFixed(2)}
+                </Text>
+                <HStack w="100%" display="flex">
+                  <Button
+                    flex={1}
+                    variant="solid"
+                    _hover={{ bg: "gray.300" }}
+                    onClick={() => router.push("/receive")}
+                  >
+                    Top-up
+                  </Button>
+                  <Button
+                    flex={1}
+                    variant="outline"
+                    color="white"
+                    _hover={{ bg: "gray.900" }}
+                    onClick={() => router.push("/pay")}
+                  >
+                    Withdraw
+                  </Button>
+                </HStack>
+              </VStack>
+            </Flex>
 
-        <TransactionList /></>
-      }
-        
+            <TransactionList userData={userData} />
+          </>
+        )}
       </VStack>
     </Flex>
   );
